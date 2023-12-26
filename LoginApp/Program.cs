f4c3648560e1a54b792492f889c78aa09e2a31d5
Hammad Malik <hammadmalik5353@gmail.com>
2023-12-26 17:43:46 +0500

    Updated

--- LoginApp/Program.cs
+++ LoginApp/Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=UserAuth}/{action=UserRegister}/{id?}");
 
 app.Run();
